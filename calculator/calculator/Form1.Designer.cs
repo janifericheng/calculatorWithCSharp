dd325a11420b3b40dd97526cff3d3cd7da0db08f
Janifer Cheng <71734101+janifericheng@users.noreply.github.com>
2021-10-05 09:36:39 -0400

    Add files via upload
    Hand-coded by Janifer I Cheng from the "Easy VB.Net, Beginners to Visual Basic App to Windows Forms" on Udemy, taught by Musavi Abdullah. Converted into C# format by Janifer Cheng.  This project is strictly for display use only with the permission of the original author of the code.

---
+++ calculator/calculator/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿
+namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnc = new System.Windows.Forms.Button();
+            this.btnper = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btndot = new System.Windows.Forms.Button();
+            this.btnsub = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btnpls = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btneq = new System.Windows.Forms.Button();
+            this.btnmult = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.firstNumLab = new System.Windows.Forms.Label();
+            this.oplabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 53);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnc
+            // 
+            this.btnc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnc.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnc.Location = new System.Drawing.Point(12, 121);
+            this.btnc.Name = "btnc";
+            this.btnc.Size = new System.Drawing.Size(154, 55);
+            this.btnc.TabIndex = 1;
+            this.btnc.Text = "C";
+            this.btnc.UseVisualStyleBackColor = true;
+            this.btnc.Click += new System.EventHandler(this.btnc_Click);
+            // 
+            // btnper
+            // 
+            this.btnper.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnper.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnper.Location = new System.Drawing.Point(175, 121);
+            this.btnper.Name = "btnper";
+            this.btnper.Size = new System.Drawing.Size(73, 55);
+            this.btnper.TabIndex = 2;
+            this.btnper.Text = "/";
+            this.btnper.UseVisualStyleBackColor = true;
+            this.btnper.Click += new System.EventHandler(this.btnper_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn9.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn9.Location = new System.Drawing.Point(172, 182);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(76, 55);
+            this.btn9.TabIndex = 4;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn6.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn6.Location = new System.Drawing.Point(172, 243);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(76, 55);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn3.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn3.Location = new System.Drawing.Point(172, 304);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(76, 54);
+            this.btn3.TabIndex = 6;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btndot
+            // 
+            this.btndot.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btndot.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btndot.Location = new System.Drawing.Point(172, 364);
+            this.btndot.Name = "btndot";
+            this.btndot.Size = new System.Drawing.Size(75, 55);
+            this.btndot.TabIndex = 7;
+            this.btndot.Text = ".";
+            this.btndot.UseVisualStyleBackColor = true;
+            this.btndot.Click += new System.EventHandler(this.btndot_Click);
+            // 
+            // btnsub
+            // 
+            this.btnsub.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnsub.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnsub.Location = new System.Drawing.Point(255, 182);
+            this.btnsub.Name = "btnsub";
+            this.btnsub.Size = new System.Drawing.Size(73, 55);
+            this.btnsub.TabIndex = 8;
+            this.btnsub.Text = "-";
+            this.btnsub.UseVisualStyleBackColor = true;
+            this.btnsub.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn8.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn8.Location = new System.Drawing.Point(92, 182);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(74, 55);
+            this.btn8.TabIndex = 9;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn5.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn5.Location = new System.Drawing.Point(92, 243);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(74, 55);
+            this.btn5.TabIndex = 10;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btnpls
+            // 
+            this.btnpls.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnpls.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnpls.Location = new System.Drawing.Point(254, 243);
+            this.btnpls.Name = "btnpls";
+            this.btnpls.Size = new System.Drawing.Size(74, 55);
+            this.btnpls.TabIndex = 11;
+            this.btnpls.Text = "+";
+            this.btnpls.UseVisualStyleBackColor = true;
+            this.btnpls.Click += new System.EventHandler(this.btnpls_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn7.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn7.Location = new System.Drawing.Point(12, 182);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(74, 55);
+            this.btn7.TabIndex = 12;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn4.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn4.Location = new System.Drawing.Point(12, 243);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(74, 55);
+            this.btn4.TabIndex = 13;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn2.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn2.Location = new System.Drawing.Point(92, 303);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(74, 55);
+            this.btn2.TabIndex = 14;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn1.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(12, 303);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(74, 55);
+            this.btn1.TabIndex = 15;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btneq
+            // 
+            this.btneq.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btneq.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btneq.Location = new System.Drawing.Point(254, 304);
+            this.btneq.Name = "btneq";
+            this.btneq.Size = new System.Drawing.Size(74, 115);
+            this.btneq.TabIndex = 16;
+            this.btneq.Text = "=";
+            this.btneq.UseVisualStyleBackColor = true;
+            this.btneq.Click += new System.EventHandler(this.btneq_Click);
+            // 
+            // btnmult
+            // 
+            this.btnmult.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnmult.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnmult.Location = new System.Drawing.Point(254, 121);
+            this.btnmult.Name = "btnmult";
+            this.btnmult.Size = new System.Drawing.Size(74, 55);
+            this.btnmult.TabIndex = 17;
+            this.btnmult.Text = "X";
+            this.btnmult.UseVisualStyleBackColor = true;
+            this.btnmult.Click += new System.EventHandler(this.btnmult_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn0.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn0.Location = new System.Drawing.Point(12, 364);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(154, 55);
+            this.btn0.TabIndex = 18;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // firstNumLab
+            // 
+            this.firstNumLab.BackColor = System.Drawing.SystemColors.Control;
+            this.firstNumLab.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.firstNumLab.ForeColor = System.Drawing.Color.Gray;
+            this.firstNumLab.Location = new System.Drawing.Point(11, 9);
+            this.firstNumLab.Name = "firstNumLab";
+            this.firstNumLab.Size = new System.Drawing.Size(274, 38);
+            this.firstNumLab.TabIndex = 19;
+            this.firstNumLab.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // oplabel
+            // 
+            this.oplabel.BackColor = System.Drawing.SystemColors.Control;
+            this.oplabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.oplabel.ForeColor = System.Drawing.Color.Gray;
+            this.oplabel.Location = new System.Drawing.Point(291, 9);
+            this.oplabel.Name = "oplabel";
+            this.oplabel.Size = new System.Drawing.Size(37, 38);
+            this.oplabel.TabIndex = 20;
+            this.oplabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 429);
+            this.Controls.Add(this.oplabel);
+            this.Controls.Add(this.firstNumLab);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnmult);
+            this.Controls.Add(this.btneq);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btnpls);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btnsub);
+            this.Controls.Add(this.btndot);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btnper);
+            this.Controls.Add(this.btnc);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnc;
+        private System.Windows.Forms.Button btnper;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btndot;
+        private System.Windows.Forms.Button btnsub;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btnpls;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btneq;
+        private System.Windows.Forms.Button btnmult;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Label firstNumLab;
+        private System.Windows.Forms.Label oplabel;
+    }
+}
+
---
+++ calculator/calculator/Form1.cs
@@ -0,0 +1,205 @@
+﻿//hand coded by Janifer I Cheng from the "Easy VB.Net, Beginners to Visual Basic App to Windows Forms"
+//on Udemy, taught by Musavi Abdullah. Converted into C# format by Janifer Cheng.
+//This project is strictly for display use only with the permission of the original author of the code.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.oplabel.Text = "-";
+            //=================
+            if (this.label1.Text == "")
+            { }
+            else
+            { this.firstNumLab.Text = this.label1.Text; }
+            //=================
+            this.label1.Text = "";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + "0";
+        }
+
+        private void btndot_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + ".";
+        }
+
+    public void clearText()
+        {
+         this.oplabel.Text = "";
+         this.firstNumLab.Text = "";
+        }
+        private void btneq_Click(object sender, EventArgs e)
+        {
+            double.TryParse(this.firstNumLab.Text, out double fn);
+            double.TryParse(this.label1.Text, out double sn);
+            double rn;
+
+
+            //if (this.oplabel.Text == "+")
+            //{
+            //    rn = fn + sn;
+            //    this.label1.Text = rn.ToString();
+            //    clearText();
+            //}
+            //else if (this.oplabel.Text == "-")
+            //{
+            //    rn = fn - sn;
+            //    this.label1.Text = rn.ToString();
+            //    clearText();
+            //}
+            //else if (this.oplabel.Text == "x")
+            //{
+            //    rn = fn * sn;
+            //    this.label1.Text = rn.ToString();
+            //    clearText();
+            //}
+            //else if (this.oplabel.Text == "/")
+            //{
+            //    rn = fn / sn;
+            //    this.label1.Text = rn.ToString();
+            //    clearText();
+            //}
+            //else
+            //{ }
+
+            //The above if/else and switch code below provide the same outcome
+
+
+
+
+            switch (oplabel.Text)
+            {
+                case "+":
+                    rn = fn + sn;
+                    this.label1.Text = rn.ToString();
+                    clearText();
+                    break;
+                case "-":
+                    rn = fn - sn;
+                    this.label1.Text = rn.ToString();
+                    clearText();
+                    break;
+                case "/":
+                    rn = fn / sn;
+                    this.label1.Text = rn.ToString();
+                    clearText();
+                    break;
+                case "x":
+                    rn = fn * sn;
+                    this.label1.Text = rn.ToString();
+                    clearText();
+                    break;
+            }
+
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + "9";
+        }
+
+        private void btnc_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = "";
+            //below functions the same as the above code
+            //this.label1.ResetText();
+        }
+
+        private void btnpls_Click(object sender, EventArgs e)
+        {
+            this.oplabel.Text = "+";
+            //=================
+            if (this.label1.Text == "")
+            {}
+            else
+            { this.firstNumLab.Text = this.label1.Text; }
+            //=================
+            this.label1.Text = "";
+        }
+
+        private void btnmult_Click(object sender, EventArgs e)
+        {
+            this.oplabel.Text = "x";
+            //=================
+            if (this.label1.Text == "")
+            { }
+            else
+            { this.firstNumLab.Text = this.label1.Text; }
+            //=================
+            this.label1.Text = "";
+        }
+
+        private void btnper_Click(object sender, EventArgs e)
+        {
+            this.oplabel.Text = "/";
+            //=================
+            if (this.label1.Text == "")
+            { }
+            else
+            { this.firstNumLab.Text = this.label1.Text; }
+            //=================
+            this.label1.Text = "";
+        }
+    }
+}
